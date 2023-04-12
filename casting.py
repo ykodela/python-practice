66c6eca25cb88d945a286bf43ac28387a9d31e1d
yamini kodela <yaminikodela@gmail.com>
2023-04-12 11:40:32 +0530

    Adding the casting file

---
+++ casting.py
@@ -0,0 +1,16 @@
+x = int(1)   
+y = int(2.8) 
+z = int("3") 
+
+#Floats
+
+x = float(1)     
+y = float(2.8)   
+z = float("3")   
+w = float("4.2") 
+
+#Strings
+
+x = str("s1") 
+y = str(2)    
+z = str(3.0)  
