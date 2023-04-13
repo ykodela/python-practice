4ec60209e4482dcca2643e4cf38283b757c92e41
yamini kodela <yaminikodela@gmail.com>
2023-04-13 13:46:13 +0530

    Add sets in python

---
+++ sets.py/eg1.py
@@ -0,0 +1,33 @@
+myset = {"apple", "banana", "cherry"}
+print(myset)
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
