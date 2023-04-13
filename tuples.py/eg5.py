deb55b41008ef6f340546bd974a3f024ed6dda79
yamini kodela <yaminikodela@gmail.com>
2023-04-13 13:21:42 +0530

    Add tuples

---
+++ tuples.py/eg5.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+ print(thistuple[i])
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
