de9c6c573875f05894567e3032c572bc065840d6
yamini kodela <yaminikodela@gmail.com>
2023-04-13 15:17:23 +0530

    Adding arrays

---
+++ arrays/eg1.py
@@ -0,0 +1,17 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+
+
+
---
+++ arrays/eg2.py
@@ -0,0 +1,21 @@
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+  print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.pop(1)
+
+print(cars)
+cars.append("Honda")
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+print(cars)
+
