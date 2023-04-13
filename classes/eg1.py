c2578462df3e47fda51757cb43e80af7514155fa
yamini kodela <yaminikodela@gmail.com>
2023-04-13 15:30:01 +0530

    Adding classes and objects

---
+++ classes/eg1.py
@@ -0,0 +1,8 @@
+class MyClass:
+  x = 5
+print(MyClass)
+
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
---
+++ classes/eg3.py
@@ -0,0 +1,6 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1 = Person("John", 36)
+print(p1)
